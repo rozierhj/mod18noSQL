dc0ea366aba55c29756be63441b92b72d3442fe8
rozierhj <160989130+rozierhj@users.noreply.github.com>
2024-07-22 18:15:35 -0400

    added email validator

--- models/User.js
+++ models/User.js
@@ -1,13 +1,21 @@
 const {Schema, model} = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new Schema({
     username: {type: String, 
         required: true, 
         unique: true,
+        trim: true,
     },
     email: {type: String, 
         required: true, 
         unique: true},
+        validate:{
+            validator: function (v) {
+                return validator.isEmail(v);
+            },
+            message: props => `${props.value} is not a valid email address`
+        },
     thoughts:[
         {
         type: Schema.Types.ObjectId,
